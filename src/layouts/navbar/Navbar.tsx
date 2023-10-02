3bbcec0d3cb3cabfc1593d3fecc7d0f00a03120e
kikeai <lg5967133@gmail.com>
2023-10-02 14:25:03 -0400

    agrego boton menu responsive

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -2,21 +2,26 @@ import List from './List'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo-blue.svg'
 import { list1, list2 } from './listsInfo'
+import MenuIcon from '../../assets/componentSVG/menu'
 
 const Navbar = () => {
   const listStyles = 'gap-16 items-center hidden xl:flex'
   const linksStyles = 'font-noto font-medium text-lg text-whiteI hover:text-yellow-500 transition-all duration-200'
 
   return (
-    <nav className='flex justify-center px-28 h-20 bg-blueI'>
+    <nav className='flex relative justify-center items-center px-28 h-20 bg-blueI'>
 
       <List items={list1} listStyle={listStyles} itemStyle={linksStyles} />
 
-      <Link className='mt-4 mx-10' to='/'>
+      <Link className='mt-20 mx-10' to='/'>
         <img className='min-w-[128px]' src={Logo} alt="logo-blue" />
       </Link>
 
       <List items={list2} listStyle={listStyles} itemStyle={linksStyles} />
+
+      <div className='absolute right-14 stroke-whiteI hover:stroke-yellow-500 transition-all duration-200 hover:cursor-pointer xl:hidden'>
+        <MenuIcon />
+      </div>
 
     </nav>
   )
