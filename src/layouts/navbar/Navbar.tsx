095c8c1418ff5ba702a9c841cd3e30bf1b6303df
kikeai <lg5967133@gmail.com>
2023-10-03 18:07:32 -0400

    hecho el responsive para el carrousel del inicio

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -16,15 +16,15 @@ const Navbar = () => {
 
       <List items={list1} listStyle={listStyles} itemStyle={linksStyles} />
 
-      <Link className='mt-20 mx-10' to='/'>
-        <img className='min-w-[128px]' src={Logo} alt="logo-blue" />
+      <Link className='mt-16 lg:mt-20 mx-10' to='/'>
+        <img className='min-w-[110px] lg:min-w-[128px]' src={Logo} alt="logo-blue" />
       </Link>
 
       <List items={list2} listStyle={listStyles} itemStyle={linksStyles} />
 
       <div
       onClick={() => dispatch(OpenCloseSidePanel())}
-      className='absolute right-14 stroke-whiteI hover:stroke-yellow-500 transition-all duration-200 hover:cursor-pointer xl:hidden'>
+      className='absolute right-8 lg:right-14 stroke-whiteI hover:stroke-yellow-500 transition-all duration-200 hover:cursor-pointer xl:hidden'>
         <MenuIcon />
       </div>
 
--- src/pages/home/Slide.tsx
+++ src/pages/home/Slide.tsx
@@ -6,10 +6,10 @@ const Slide = ({ title, content, image }: Preach) => {
 
       <div className='w-full h-[600px] absolute top-0 right-0 max-w-[1440px] bg-gradient-to-t from-black'></div>
 
-      <div className='absolute left-24 bottom-16 '>
-        <h2 className='font-noto font-black text-whiteI text-4xl'>{title}</h2>
-        <p className='font-noto font-extralight text-whiteI text-sm max-w-[850px] mt-5'>{content}</p>
-        <p className='font-noto font-bold text-whiteI mt-5 underline w-max hover:text-blue-700 hover:cursor-pointer transition-all duration-300'>Leer más</p>
+      <div className='absolute left-10 bottom-8 lg:left-24 lg:bottom-16 '>
+        <h2 className='font-noto font-black text-whiteI text-xl mr-10 lg:mr-0 lg:text-4xl'>{title}</h2>
+        <p className='font-noto font-extralight line-clamp-4 text-whiteI text-xs lg:text-sm max-w-[850px] mt-1 mr-10 lg:mr-0 lg:mt-5'>{content}</p>
+        <p className='font-noto font-bold text-whiteI text-sm lg:text-base mt-2 mb-10 lg:mt-5 underline w-max hover:text-blue-700 hover:cursor-pointer transition-all duration-300'>Leer más</p>
       </div>
 
       <img
