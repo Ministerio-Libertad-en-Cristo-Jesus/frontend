97f4881d93c0206fe68aeac07cb64e8493b4d580
kikeai <lg5967133@gmail.com>
2023-10-05 15:56:06 -0400

    agregado el responsive de la seccion de ofrendas

--- src/pages/home/PreachsSection/Cards.tsx
+++ src/pages/home/PreachsSection/Cards.tsx
@@ -2,17 +2,24 @@ import Button from '../../../components/Button'
 import { type Preaches } from '../../../types/types'
 import Card from './Card'
 
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/pagination'
+
 const Cards = ({ preaches }: Preaches) => {
   return (
     <div className='flex flex-col items-center mb-10 lg:mb-24 border-2 rounded-3xl border-blueI mx-10 lg:mx-24 p-6 lg:p-12 max-w-[1440px]'>
+
       <div className='flex flex-wrap gap-12 justify-center lg:gap-20'>
         {preaches.map((preach) => (
           <Card {...preach} key={preach.id} />
         ))}
       </div>
+
       <div className='mt-12'>
         <Button text='Ver más Prédicas' dark={false} onClick={() => {}} />
       </div>
+
     </div>
   )
 }
